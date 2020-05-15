d73b5a86da5a4c35b4c7a59e5e0828e42f771924
kczan <fchrzan@gmail.com>
2020-05-15 20:40:41 +0200

    Prepared for first deployment

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,6 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 from .models import Tweet
 
 # Create your tests here.
+User = get_user_model()
 
-class TweetTestCase(TestCase)
+class TweetTestCase(TestCase):
+  def setUp(self):
+    self.user = User.objects.create_user(username='test', password='password')
+
+  def test_tweet_created(self):
+    tweet = Tweet.objects.create(content='My tweet', author=self.user)
+    self.assertEqual(tweet.id, 1)
+    self.assertEqual(tweet.author, self.user)
